bd2a912be0d434a1a0eed0086b60153b4c6b7200
SunDwarf <eyesismine@gmail.com>
2015-04-23 22:20:18 +0100

    Update cfg with default values

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 config = {
-    "host": "tf.navalservers.com",
+    # The hostname of your server.
+    "host": "tf.example.com",
+    # The user for the app to login as.
     "user": "srcds",
+    # The FTP password.
+    "ftp_pass": "password",
+    # The method to login as.
+    # SSH is more secure and more powerful.
+    # FTP is supported on nearly all hosts.
     "connection_agent": "ssh",
-    "server_root": "/home/srcds/servers/suijin/tf"
+    # The root directory for the server.
+    "server_root": "/home/srcds/tf2/tf"
 }
